3de13fd427ac47a6c13faea90f290045ac62983b
Max Rebryk <rebryk2003@gmail.com>
2024-03-06 19:40:58 +0300

    task1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,1 +1,24 @@
 "use strict";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+export function renderPhoto(photo) {
+  const gallery = document.querySelector(".gallery");
+  let markup = "";
+  for (let key in photo.hits){
+    markup += `<ul class="gallery-ul">
+        <li class="gallery-item">
+        <img class="photo-img" src="${photo.hits[key].largeImageURL}">
+        <h3 class="photo-header">Likes</h3>
+        <p class="photo-value">${photo.hits[key].likes}</p>
+        <h3 class="photo-header">Views</h3>
+        <p class="photo-value">${photo.hits[key].views}</p>
+        <h3 class="photo-header">Comments</h3>
+        <p class="photo-value">${photo.hits[key].comments}</p>
+        <h3 class="photo-header">Downloads</h3>
+        <p class="photo-value">${photo.hits[key].downloads}</p>
+        <li>
+      </ul>`;
+  }
+  gallery.insertAdjacentHTML("afterbegin", markup);
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 "use strict";
-
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 import * as  httpFunc from  "./js/pixabay-api.js";
 import * as render from "./js/render-functions.js";
 
 const searchInput = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-btn");
 
+
 searchButton.addEventListener("click", (event) =>{
+    event.preventDefault();
+    const searchWord =  searchInput.value;
     if(searchInput.value === ""){
         alert("Search input is empty!")
     }
+    else{
+        httpFunc.fetchImages(searchWord)
+        .then((images) => render.renderPhoto(images))
+        .catch((error) => console.log(error));
+    }
+
 })
+
